451951eae96a8be9f2462b326c846b738c456b05
Tom Kunc <tomkunc0@gmail.com>
2018-06-11 14:37:17 +1000

    fix(cse_source): booleans are 0

--- cse_source.sh
+++ cse_source.sh
@@ -8,10 +8,10 @@ DIR="$(dirname $([ -L $0 ] && readlink -f $0 || echo $0))"
 source "${DIR}/utils.sh"
 
 HOST="$(lower `hostname --long`)"
-IS_LOCAL=0
+IS_LOCAL=1
 
 if [[ "${HOST}" = *"cse.unsw.edu.au" ]]; then
-    IS_LOCAL=1
+    IS_LOCAL=0
     source "${DIR}/autocomplete/autocomplete_local.sh"
     source "${DIR}/cse_local.sh"
 else
