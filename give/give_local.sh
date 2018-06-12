2a8689ec8542080aabc54ba3850bd03fa5bd3866
Tom Kunc <tomkunc0@gmail.com>
2018-06-12 23:38:13 +1000

    fix(gives): remove magic of gives, by request of CSE

--- give/give_local.sh
+++ give/give_local.sh
@@ -1,6 +1,4 @@
 gives(){
-    echo "WARNING: by running this program you are agreeing to the terms of the "give" program."
-    echo "You should review them and consent to them before using this program!"
-    echo "yes\nyes\n" | give "$@"
+    echo "" | give "$@"
 }
 complete -F _give gives
